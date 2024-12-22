ca0ab4c786552caf626091eeca1e327ae97d42d9
FelipeReina12 <jfreinap@academia.usbbog.edu.co>
2024-12-22 13:31:19 -0500

    Continuando con el curso

---
+++ while_loop.py
@@ -0,0 +1,3 @@
+user_name = " "
+while user_name != "pypy":  #Esto significa que el usuario debe ingresar "pypy" para que el juego comience
+    user_name = input("Enter your name: ")  #De lo contrario , el juego no comienza
