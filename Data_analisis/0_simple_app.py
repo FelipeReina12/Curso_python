15026d1cc9f73170f6a3d3db0081e7a6710a339d
FelipeReina12 <jfreinap@academia.usbbog.edu.co>
2025-02-17 07:44:15 -0500

    Por corregir

--- Data_analisis/0_simple_app.py
+++ Data_analisis/0_simple_app.py
@@ -1,10 +1,79 @@
 import justpy as jp
+#Pagina para ver las funciones de juspy "Highcharts Documentation"
+#Para buscar los tipos de lines vamos a Chart and series types
+
+chart_def = """
+{
+    chart: {
+        type: 'spline',
+        inverted: true
+    },
+    title: {
+        text: 'Atmosphere Temperature by Altitude'
+    },
+    subtitle: {
+        text: 'According to the Standard Atmosphere Model'
+    },
+    xAxis: {
+        reversed: false,
+        title: {
+            enabled: true,
+            text: 'Altitude'
+        },
+        labels: {
+            format: '{value} km'
+        },
+        accessibility: {
+            rangeDescription: 'Range: 0 to 80 km.'
+        },
+        maxPadding: 0.05,
+        showLastLabel: true
+    },
+    yAxis: {
+        title: {
+            text: 'Temperature'
+        },
+        labels: {
+            format: '{value}°'
+        },
+        accessibility: {
+            rangeDescription: 'Range: -90°C to 20°C.'
+        },
+        lineWidth: 2
+    },
+    legend: {
+        enabled: false
+    },
+    tooltip: {
+        headerFormat: '<b>{series.name}</b><br/>',
+        pointFormat: '{point.x} km: {point.y}°C'
+    },
+    plotOptions: {
+        spline: {
+            marker: {
+                enable: false
+            }
+        }
+    },
+    series: [{
+        name: 'Temperature',
+        data: [
+            [0, 15], [10, -50], [20, -56.5], [30, -46.5], [40, -22.1],
+            [50, -2.5], [60, -27.7], [70, -55.7], [80, -76.5]
+        ]
+
+    }]
+}
+"""
 
 def app():
     wp = jp.QuasarPage()
     h1 = jp.QDiv(a = wp, text= "Analisys of course Reviews", 
                  classes= "text-h4 text-center q-pa-md")
     p1 = jp.QDiv(a= wp, text= "These graphs represent course review analysis")
+    hc = jp.HighCharts(a = wp, options= chart_def)
+    print(hc.options)
+    print(type(hc.options))
     return wp
 
 jp.justpy(app)
