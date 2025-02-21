5fcb766ad676752bcc30302aac4e5c5ca2b06907
FelipeReina12 <jfreinap@academia.usbbog.edu.co>
2025-02-21 11:16:29 -0500

    Pagina web

--- App_3_web_page/script_1.py
+++ App_3_web_page/script_1.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-
+@app.route("/")  #Para ver la pagina buscamos "localhost:5000/"
 def home():
-    return "Website content goes here!"  #Para ver la pagina buscamos "localhost:5000"
+    return "Home page here!"
+
+@app.route("/about")  #Para ver la pagina buscamos "localhost:5000/about"
+def about():
+    return "About content goes here!"  
 
 if __name__ == "__main__":
     app.run(debug= True)
