4f0be45b888d688840a5638776d671d4c9d23cd3
FelipeReina12 <jfreinap@academia.usbbog.edu.co>
2025-06-05 14:59:12 -0500

    Conectar bases de datos

--- App_4_Interfases/script2.py
+++ App_4_Interfases/script2.py
@@ -1,21 +1,21 @@
-import sqlite3
+import psycopg2
 
 def create_table():  # Función para crear la tabla
-    conn = sqlite3.connect("lite.db")  # Conexión con la base de datos
+    conn = psycopg2.connect("dbname = 'Database 1' user = 'postgres' password = 'postgres123' host = 'localhost' port = '5432' ")  # Conexión con la base de datos
     cur = conn.cursor()  # Cursor para ejecutar sentencias SQL
     cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)")  # Se crea la tabla con columnas correctas
     conn.commit()  # Guardar los cambios
     conn.close()  # Cerrar la conexión
 
 def insert(item, quantity, price):  # Función para insertar datos
-    conn = sqlite3.connect("lite.db")
+    conn = psycopg2.connect("dbname = 'Database 1' user = 'postgres' password = 'postgres123' host = 'localhost' port = '5432' ")
     cur = conn.cursor()
-    cur.execute("INSERT INTO store (item, quantity, price) VALUES (?, ?, ?)", (item, quantity, price))  # Insertar datos con nombres de columnas
+    cur.execute("INSERT INTO store (item, quantity, price) VALUES ('%s', '%s', '%s')" % (item, quantity, price))  # Insertar datos con nombres de columnas
     conn.commit()
     conn.close()
 
 def view():  # Función para mostrar los datos de la tabla
-    conn = sqlite3.connect("lite.db")
+    conn = psycopg2.connect("dbname = 'Database 1' user = 'postgres' password = 'postgres123' host = 'localhost' port = '5432' ")
     cur = conn.cursor()
     cur.execute("SELECT * FROM store")
     rows = cur.fetchall()
@@ -23,16 +23,16 @@ def view():  # Función para mostrar los datos de la tabla
     return rows
 
 def delete(item):  # Función para borrar los datos de la tabla
-    conn = sqlite3.connect("lite.db")
+    conn = psycopg2.connect("dbname = 'Database 1' user = 'postgres' password = 'postgres123' host = 'localhost' port = '5432' ")
     cur = conn.cursor()
-    cur.execute("DELETE FROM store WHERE item = ?", (item,))
+    cur.execute("DELETE FROM store WHERE item = %s", (item,))
     conn.commit()
     conn.close()
 
 def update(quantity, price, item):  # Función para borrar los datos de la tabla
-    conn = sqlite3.connect("lite.db")
+    conn = psycopg2.connect("dbname = 'Database 1' user = 'postgres' password = 'postgres123' host = 'localhost' port = '5432' ")
     cur = conn.cursor()
-    cur.execute("UPDATE store SET quantity = ?, price = ? WHERE item = ?", (quantity, price, item))
+    cur.execute("UPDATE store SET quantity = %s, price = %s WHERE item = %s", (quantity, price, item))
     conn.commit()
     conn.close()
 
@@ -42,10 +42,10 @@ def update(quantity, price, item):  # Función para borrar los datos de la tabla
 
 # Crear la tabla y agregar un registro
 create_table()
-# insert("Coffe Cup", 10, 5)
+# # insert("Apple", 10, 5)
 # insert("Watter", 15, 2)
 update(20, 4, "Watter")
-# delete("Watter")
+# delete("Apple")
 # delete("Coffe Cup")
 # Mostrar los registros
-print(view())
+# print(view())
