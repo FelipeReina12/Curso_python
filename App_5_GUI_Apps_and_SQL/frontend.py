5bd91838622cc7f383bbe6115444089e37302624
FelipeReina12 <jfreinap@academia.usbbog.edu.co>
2025-06-07 12:20:11 -0500

    Terminado

--- App_5_GUI_Apps_and_SQL/frontend.py
+++ App_5_GUI_Apps_and_SQL/frontend.py
@@ -3,8 +3,17 @@ import backend
 
 def get_selected_row(event):
     global selected_tuple
-    index = list1.curselection()[0]
-    selected_tuple = list1.get(index)
+    if list1.curselection():  # Verifica si hay una selección
+        index = list1.curselection()[0]
+        selected_tuple = list1.get(index)
+        e1.delete(0, END)
+        e1.insert(END, selected_tuple[1])
+        e2.delete(0, END)
+        e2.insert(END, selected_tuple[2])
+        e3.delete(0, END)
+        e3.insert(END, selected_tuple[3])
+        e4.delete(0, END)
+        e4.insert(END, selected_tuple[4])
 
 def view_command():
     list1.delete(0, END)
@@ -23,11 +32,12 @@ def add_command():
 def delete_command():
     backend.delete(selected_tuple[0])
 
-
-
-
+def update_command():
+    backend.update(selected_tuple[0],title_text.get(), author_text.get(), year_text.get(), isbn_text.get())
 
 window = Tk()  #Aqui lo que hacemos es crear una ventana
+
+window.wm_title("BookStore")
 
 #Creamos los labels y les asiganamos un texto y una posición
 l1 = Label(window, text= "Title")  
@@ -56,8 +66,8 @@ e3 = Entry(window, textvariable=year_text)
 e3.grid(row= 1, column= 1)
 
 isbn_text = StringVar()
-e1 = Entry(window, textvariable=isbn_text)
-e1.grid(row= 1, column= 3)
+e4 = Entry(window, textvariable=isbn_text)
+e4.grid(row= 1, column= 3)
 
 #Agregamos la lista de libros
 list1 = Listbox(window, height=6, width=35)
@@ -82,13 +92,13 @@ b2.grid(row= 3, column=3)
 b3 = Button(window, text= "Add entry", width= 12, command= add_command)
 b3.grid(row= 4, column= 3)
 
-b4 = Button(window, text= "Update", width= 12)
+b4 = Button(window, text= "Update", width= 12, command= update_command)
 b4.grid(row= 5, column=3)
 
 b5 = Button(window, text= "Delete", width= 12, command= delete_command)
 b5.grid(row= 6, column= 3)
 
-b6 = Button(window, text= "Close", width= 12)
+b6 = Button(window, text= "Close", width= 12, command= window.destroy)
 b6.grid(row= 7, column=3)
 
 
