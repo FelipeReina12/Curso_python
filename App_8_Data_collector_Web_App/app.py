a52f1823145ea9a881ec17b82f5f6d6020d6350b
Felipe Reina <jfreinap@academia.usbbog.edu.co>
2025-09-19 13:32:00 -0500

    App sin funcinar

--- App_8_Data_collector_Web_App/app.py
+++ App_8_Data_collector_Web_App/app.py
@@ -1,1 +1,14 @@
 from flask import Flask, request, jsonify
+
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
+
