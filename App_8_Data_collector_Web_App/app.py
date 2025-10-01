5575fb858b30a248d5e859b873f3559f7a1948d8
Felipe Reina <jfreinap@academia.usbbog.edu.co>
2025-09-30 22:50:38 -0500

    Funcionando perfectamente

--- App_8_Data_collector_Web_App/app.py
+++ App_8_Data_collector_Web_App/app.py
@@ -26,10 +26,12 @@ def success():
         email = request.form["email_name"]
         height = request.form["height_name"]
         print(f"email: {email}", f"height: {height}")
-        data = Data(email, height) 
-        db.session.add(data)
-        db.session.commit()
-        return render_template("success.html")
+        if (db.session.query(Data).filter(Data.email_ == email).count() == 0):
+            data = Data(email, height) 
+            db.session.add(data)
+            db.session.commit()
+            return render_template("success.html")
+        return render_template("index.html", text="Seems like we've got something from that email address already!")
 
 if __name__ == "__main__":
     with app.app_context():
