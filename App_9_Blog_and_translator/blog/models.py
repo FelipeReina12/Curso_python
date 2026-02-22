08b4d64cb119c24acf2fa6a786d65bf60d338d32
FelipeReina12 <jfreinap@academia.usbbog.edu.co>
2026-02-22 13:42:12 -0500

    Coninue....

---
+++ App_9_Blog_and_translator/blog/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    data_created = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(max_length=200,unique=True)
+    author = models.ForeignKey(to=User(), on_delete=models.CASCADE, )
---
+++ App_9_Blog_and_translator/readme.txt
@@ -0,0 +1,7 @@
+python manage.py runserver
+python manage.py createsuperuser
+python manage.py startapp name
+
+Django Admin
+username: felipe_reina12
+password: coki21Ta.
