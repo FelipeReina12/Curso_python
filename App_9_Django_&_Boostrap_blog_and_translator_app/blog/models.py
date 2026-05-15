fbc5dab2a1ee07761a18d29f6cfc5ee7d9b3b084
FelipeReina12 <jfreinap@academia.usbbog.edu.co>
2026-05-15 10:16:44 -0500

    Creando la base de datos

--- App_9_Django_&_Boostrap_blog_and_translator_app/blog/models.py
+++ App_9_Django_&_Boostrap_blog_and_translator_app/blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User  # Importamos el modelo de usuario de Django para relacionarlo con el autor de la publicación
 
+STATUS = ((0, 'Draft'), (1, 'Publish'))  # Definimos una tupla de opciones para el campo de estado de la publicación 
 # Create your models here.
+class Post(models.Model):  # Model es la clase que representa una tabla en la base de datos.
+    title = models.CharField(max_length=200)  # CharField es un campo de texto con un límite de caracteres.
+    content = models.TextField()  # TextField es un campo de texto sin límite de caracteres.
+    date_created = models.DateTimeField(auto_now_add=True)  # DateTimeField es un campo de fecha y hora. 
+    #auto_now_add=True hace que se establezca automaticamente la hora de creación de la publicación.
+    slug = models.SlugField(max_length=200, unique=True)  # SlugField es un campo de texto que se utiliza para crear URLs amigables. 
+    #unique=True hace que el valor del slug sea únco en la base de datos.
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE)  # ForeignKey es un campo que crea una relación de clave foránea con otro modelo.
+    #to=User indica que la relación es con el modelo de usuario de Django.
+    #on_delete=models.CASCADE hace que si el usuario es eliminado, también se eliminen todas sus publicaciones.
+    status = models.IntegerField(choices=STATUS, default=0)  # IntegerField es un campo de numero entero.
+    #choices=STATUS hace que el campo solo pueda tomar los valores definidos en la tupla STATUS. 
--- App_9_Django_&_Boostrap_blog_and_translator_app/readme.md
+++ App_9_Django_&_Boostrap_blog_and_translator_app/readme.md
@@ -24,3 +24,14 @@ py manage.py startapp blog. Esto nos crea la carperta con varios archivos y migr
 Las migraciónes son un cambio en las bases de datos
 
 En el archivo models.py se enecuentran las bases de datos de la página web 
+
+## Agregar nuestra aplicación a las configuraciones de mysite
+En la carpeta mysite ubicamos el archivo settings.py y donde está la lista INSTALLED_APPS en el ultimo campo agregamso el nombre de nuestra app
+
+# Comenzando a desarrollar nuestra app
+
+## Modelos 
+En el archivo models.py organizamos los valores de la base de datos mediante la clase models   
+Luego para aplicar ese modelo a la base de datos aplicamos los siguientes comandos:  
+py manage.py makemigrations   
+py manage.py migrate
